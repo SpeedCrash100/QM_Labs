837e8b0834530dd2d217ccbc9c4a0f5c3540361d
SpeedCrash100 <SpeedCrash100@yandex.ru>
2019-10-17 07:31:19 +0300

    Remove not needed peek tests

--- labwork/src/test/java/ru/deucalion/test/whitebox/ParserContextTest.java
+++ labwork/src/test/java/ru/deucalion/test/whitebox/ParserContextTest.java
@@ -43,35 +43,4 @@ public class ParserContextTest {
 		}
 	}
 	
-	@Test
-	public void peekNormal()
-	{
-		ParserContext ctx = new ParserContext("qfasa");
-		try {			
-			char c = ctx.peek();
-			assertEquals(c, 'q');
-			assertEquals(-1, ctx.getCurrentPosition());
-		} catch (IOException e) {
-			fail("Exception has thrown : " + e.getMessage());
-		}
-	}
-	
-	@Test
-	public void peekAtEnd()
-	{
-		ParserContext ctx = new ParserContext("q");
-		
-		try {
-			ctx.read();
-		} catch (IOException e1) {
-			fail("ParserContext::read works invalid");
-		}
-
-		try {			
-			ctx.peek();
-			fail("Expected IOException");
-		} catch (IOException e) {
-			assertEquals("Cannot read further!", e.getMessage());
-		}
-	}
 }
