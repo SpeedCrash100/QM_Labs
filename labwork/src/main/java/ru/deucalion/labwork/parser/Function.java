bc15d2036117f7965f1181ab7ab8bc9b591f6129
SpeedCrash100 <SpeedCrash100@yandex.ru>
2020-05-11 12:12:04 +0300

    Class for functions

---
+++ labwork/src/main/java/ru/deucalion/labwork/parser/Function.java
@@ -0,0 +1,13 @@
+package ru.deucalion.labwork.parser;
+
+import java.io.IOException;
+
+public class Function extends AbstractExpression {
+
+	@Override
+	public void parse(ParserContext context) throws IOException {
+		//TODO! parse functions like sin, cos, tg, ctg
+		return;
+	}
+
+}
