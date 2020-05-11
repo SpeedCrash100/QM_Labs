343513d7d5fd762b03bcf1af86fd12f1c46de215
SpeedCrash100 <SpeedCrash100@yandex.ru>
2020-05-11 12:13:30 +0300

    Floating point digit class

---
+++ labwork/src/main/java/ru/deucalion/labwork/parser/FloatingNumber.java
@@ -0,0 +1,13 @@
+package ru.deucalion.labwork.parser;
+
+import java.io.IOException;
+
+public class FloatingNumber extends AbstractExpression {
+
+	@Override
+	public void parse(ParserContext context) throws IOException {
+		// TODO parse float digit
+		return;
+	}
+
+}
