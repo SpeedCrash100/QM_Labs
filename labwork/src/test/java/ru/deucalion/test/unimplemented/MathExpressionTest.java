1a7b93a44a4be41bffb4375ee5a30c6cc464e2db
SpeedCrash100 <SpeedCrash100@yandex.ru>
2020-05-12 09:40:09 +0300

    Error check

---
+++ labwork/src/test/java/ru/deucalion/test/unimplemented/MathExpressionTest.java
@@ -0,0 +1,27 @@
+package ru.deucalion.test.unimplemented;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import ru.deucalion.labwork.App;
+
+public class MathExpressionTest {
+	
+	@Test
+	public void noOpenningBracket()
+	{
+		String testExp = "5+3)";
+		String result = App.parse(testExp);
+		assertTrue(result.startsWith("Invalid"));
+	}
+	
+	@Test
+	public void invalidSymbol()
+	{
+		String testExp = "5+3x-4";
+		String result = App.parse(testExp);
+		assertTrue(result.startsWith("Invalid"));
+	}
+
+}
