c509c44f24464e19a14d8af26678df15911e8ecf
SpeedCrash100 <SpeedCrash100@yandex.ru>
2020-05-12 07:10:04 +0300

    Basic operators

---
+++ labwork/src/main/java/ru/deucalion/labwork/parser/operators/DivideOperatorDescription.java
@@ -0,0 +1,7 @@
+package ru.deucalion.labwork.parser.operators;
+
+import ru.deucalion.labwork.parser.OperatorDescription;
+
+public class DivideOperatorDescription extends OperatorDescription {
+
+}
